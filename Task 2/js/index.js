b699f332d4257e395565b2531b51c1107cf8f6e3
mallaiv05 <maved7202@uib.no>
2026-03-12 14:12:57 +0100

    laga addTask funksjon, og resten av funksjonene

--- Task 2/js/index.js
+++ Task 2/js/index.js
@@ -1,11 +1,53 @@
 let taskList=[];
 
-function addTasks(){
+function addTask(){
     const task=document.getElementById("taskInput").value;
     const date=document.getElementById("dateInput").value;
+    const errorMessage=document.getElementById("errorMessage");
+
+    if (task.trim().length==0){
+        errorMessage.textContent="Please enter a message";
+        errorMessage.style.color='red';
+        return;
+    }
+
+    errorMessage.textContent='';
+
+    const newTask={
+        text:task,
+        dueDate:date,
+        completed:false, 
+        id:Date.now()
+    }
+    
+    taskList.push(newTask);
+
+    document.getElementById('taskInput').value="";
+    document.getElementById('dateInput').value="";
+    
+
+}
+
+document.getElementById("addBtn").addEventListener("click", addTask);
+
+
+function sortTasks(){
+    
+}
+function renderTasks(){
+
+}
+
+function createTaskElements(){
+
 }
 
 function removeItem(){
 
 }
+
+function toggleTask(id){
+
+}
+
 
