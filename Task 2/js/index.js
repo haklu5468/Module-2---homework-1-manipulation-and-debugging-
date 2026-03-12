9eb19248a204bdb74e90701e3fa883a68b1dcef6
mallaiv05 <maved7202@uib.no>
2026-03-12 13:13:52 +0100

    lagt til 2 funksjoner

--- Task 2/js/index.js
+++ Task 2/js/index.js
@@ -1,9 +1,11 @@
 let taskList=[];
 
 function addTasks(){
-
+    const task=document.getElementById("taskInput").value;
+    const date=document.getElementById("dateInput").value;
 }
 
 function removeItem(){
-    
+
 }
+
