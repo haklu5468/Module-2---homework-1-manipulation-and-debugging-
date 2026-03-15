46fb892f12f32ea371081c93260da7f3c2f8e12f
mallaiv05 <maved7202@uib.no>
2026-03-15 10:27:42 +0100

    lagd sortTasks funksjon

--- Task 2/js/index.js
+++ Task 2/js/index.js
@@ -37,6 +37,23 @@ function addTask(){
 
 
 function sortTasks(){
+    tasks.sort(function(a,b){
+
+        if(a.dueDate==="" && b.dueDate===""){
+            return 0;
+        }
+
+        if(a.dueDate===""){
+            return 1;
+        }
+
+        if(b.dueDate===""){
+            return -1;
+        }
+        
+        return new Date(a.dueDate)-new Date(b.dueDate);
+    });
+    
     //sorterer tasks etter dato
 }
 
